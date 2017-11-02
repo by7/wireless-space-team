ce4c9614899f5c6a50b822ea4f63852645f1fa23
Ben Yang <thisisbenbtw@gmail.com>
2017-11-02 13:56:18 -0700

    Fixed test transmitter code

--- test_RF24/test_RF24.ino
+++ test_RF24/test_RF24.ino
@@ -13,22 +13,18 @@ void setup() {
   rf.begin();
   rf.setChannel(5);
   rf.setPALevel(RF24_PA_MIN);
-  rf.openWritingPipe(0xc2c2c2c2c2); //what do i put here
-  rf.openReadingPipe(1,0xe7e7e7e7e7);
+  rf.openReadingPipe(0,0xe7e7e7e7e7);
   rf.setCRCLength(RF24_CRC_16);
-  pinMode(CSN,OUTPUT);
-  pinMode(CE,OUTPUT);
+  rf.startListening();
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rf.startListening();
   char buf = '0';
   if(rf.available()){
     rf.read(&buf,sizeof(buf));
+    Serial.println(buf);
   }
-  Serial.println(buf);
-  rf.stopListening();
   delay(100);
 }
--- test_RF24_2/test_RF24_2.ino
+++ test_RF24_2/test_RF24_2.ino
@@ -15,24 +15,22 @@ void setup() {
   rf.setChannel(5);
   rf.setPALevel(RF24_PA_MIN);
   rf.openWritingPipe(0xe7e7e7e7e7); //what do i put here
-  rf.openReadingPipe(1,0xc2c2c2c2c2);
   rf.setCRCLength(RF24_CRC_16);
   pinMode(BTN,INPUT);
-  pinMode(CSN,OUTPUT);
-  pinMode(CE,OUTPUT);
+  rf.stopListening();
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int in = digitalRead(BTN);
-  rf.startListening();
   char buf = '1';
   if(in == HIGH){
-    rf.write(&buf,1);
+    Serial.println(buf);
+    if(!rf.write(&buf,1)){
+      Serial.println("No ack");
+    }
   }
-  Serial.println(buf);
-  rf.stopListening();
   
   delay(100);
 }
