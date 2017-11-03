f6aaf36b584a140b5573d3632df32f257c8f81f3
Amy Tu <tu.amy.a@gmail.com>
2017-11-02 18:06:33 -0700

    ...

--- Arduino_side/Arduino_side.ino
+++ Arduino_side/Arduino_side.ino
@@ -5,7 +5,9 @@
 
 #define CSN 10
 #define CE 9
-#define BTN 3
+#define BTN_R 3
+#define BTN_G 4
+#define BTN_B 5
 
 RF24 rf(CE, CSN);
 void setup() {
@@ -16,31 +18,38 @@ void setup() {
   rf.setPALevel(RF24_PA_MIN);
   rf.openWritingPipe(0xe7e7e7e7e7); //what do i put here
   rf.setCRCLength(RF24_CRC_16);
-  pinMode(BTN,INPUT);
+  pinMode(BTN_R,INPUT);
+  pinMode(BTN_G,INPUT);
+  pinMode(BTN_B,INPUT);
   rf.stopListening();
   Serial.begin(9600);
   rf.printDetails();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int in = digitalRead(BTN);
-  char buf = '!';
-//  if(in == HIGH){ //input to button
-//    Serial.println(buf);
-//    if(!rf.write(&buf,1)){
-//      Serial.println("No ack");
-//    } else {
-//      Serial.println("ack");
-//      }
-//  } else { //no input to button
-//    Serial.println("No button presso");
-//  }
-  if (rf.write(&buf, 1)) {
-    Serial.println("We in business, bitches");  
-  } else {
-    Serial.println("We ain't doing so hot, sweetharht");
+  //get inputs for the buttons
+  int red = digitalRead(BTN_R);
+  int green = digitalRead(BTN_G);
+  int blue = digitalRead(BTN_B);
+  
+  //what to send when buttons are pressed
+  char r = 'r';
+  char g = 'g';
+  char b = 'b';
+  
+  if (red == HIGH) { //if red send r
+    rf.write(&r, 1);
+  } else if (green == HIGH) { //if green send g
+    rf.write(&g, 1);
+  } else if (blue == HIGH) { //if blue send b
+    rf.write(&b, 1);
   }
+  
+//  if (rf.write(&buf, 1)) {
+//    Serial.println("We in business, bitches");  
+//  } else {
+//    Serial.println("We ain't doing so hot, sweetharht");
+//  }
   
   Serial.println("FUCK.");
   
