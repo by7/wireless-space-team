2fcd35033425b3eca62fdfab014f73684a10763b
Amy Tu <tu.amy.a@gmail.com>
2017-11-02 18:48:50 -0700

    .

--- Arduino_side/Arduino_side.ino
+++ Arduino_side/Arduino_side.ino
@@ -9,6 +9,11 @@
 #define BTN_G 4
 #define BTN_B 5
 
+struct Sequence {
+  int len;
+  char* colors;  
+} seqArduino;
+
 RF24 rf(CE, CSN);
 void setup() {
   // put your setup code here, to run once:
@@ -16,42 +21,54 @@ void setup() {
   rf.begin();
   rf.setChannel(5);
   rf.setPALevel(RF24_PA_MIN);
-  rf.openWritingPipe(0xe7e7e7e7e7); //what do i put here
+  rf.openWritingPipe(0xc2c2c2c2c2); //what do i put here
+  rf.openReadingPipe(1s, 0xe7e7e7e7e7);
   rf.setCRCLength(RF24_CRC_16);
+  
   pinMode(BTN_R,INPUT);
   pinMode(BTN_G,INPUT);
   pinMode(BTN_B,INPUT);
-  rf.stopListening();
+  //rf.stopListening();
   Serial.begin(9600);
-  rf.printDetails();
+  //rf.printDetails();
+
+  seqArduino.len = 0; //init sequence
+  seqArduino.colors = malloc(0);
 }
+
+int readlen = 0;
+bool correct = true;
+int indexArduino = 0, indexTeensy= 0; //place in array in Arduino and Teensy
 
 void loop() {
   //get inputs for the buttons
   int red = digitalRead(BTN_R);
   int green = digitalRead(BTN_G);
   int blue = digitalRead(BTN_B);
+
+  char r = 'R', g = 'G', b = 'B';
   
-  //what to send when buttons are pressed
-  char r = 'r';
-  char g = 'g';
-  char b = 'b';
+  //reads sequence if new cycle and shit
+  seqArduino.len = seq.len;
+  realloc(seqArduino.colors, sizeof(char)*seq.len);
   
-  if (red == HIGH) { //if red send r
-    rf.write(&r, 1);
-  } else if (green == HIGH) { //if green send g
-    rf.write(&g, 1);
-  } else if (blue == HIGH) { //if blue send b
-    rf.write(&b, 1);
+  //get button presses 
+  if (red == HIGH) {
+    seqArduino.colors[indexArduino] = 'R';
+  } else if (green == HIGH) {
+    seqArduino.colors[indexArduino] = 'R';
+  } else if (blue == HIGH) {
+    
   }
-  
-//  if (rf.write(&buf, 1)) {
-//    Serial.println("We in business, bitches");  
-//  } else {
-//    Serial.println("We ain't doing so hot, sweetharht");
-//  }
-  
-  Serial.println("FUCK.");
+
+  //if button press does not match with teensy then send false and deallocate
+  //or is length is same and button presses are correct then send true and deallocate
   
   delay(100);
 }
+
+
+
+
+
+
